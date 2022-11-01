d81f0b0767ebd27cd89e322451085cb1db482b72
streanger <pslisz@o2.pl>
2022-11-01 22:17:17 +0100

    fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    
+
 setuptools.setup(
     name='duplicate',
     version='0.1.0',
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/streanger/duplicate",
     packages=['duplicate',],
-    install_requires=['pyperclip', 'rich'],
+    install_requires=['rich', 'Send2Trash'],
     include_package_data=True,
     package_data={},
     entry_points={
